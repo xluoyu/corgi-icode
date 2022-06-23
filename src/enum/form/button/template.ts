66c9b6cf4b8691041df6964eef754d00560a1403
xluoyu <llzz0895@163.com>
2022-06-23 17:22:44 +0800

    add button code template

---
+++ src/enum/form/button/template.ts
@@ -0,0 +1,11 @@
+import type { IFormItemOptions } from '../type'
+import { formatArrt } from '@/utils/renderTemplate'
+
+export default function(options: IFormItemOptions) {
+  const attrs = ['type', 'size']
+  const attrsStr = attrs.map(attr => formatArrt(attr, options[attr].value)).filter(Boolean).join(' ')
+
+  return {
+    template: `<el-button ${attrsStr}${!options.inline.value ? ' style="display: flex"' : ''}>${options.content}</el-button>`,
+  }
+}
